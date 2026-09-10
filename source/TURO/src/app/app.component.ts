c596993ceb7680b651019bf06ec5fde583e1dfc7
Mattéo LOPES <matteo.lopes.lycee@gmail.com>
2026-09-10 09:52:16 +0200

    apprentissage des pipes et des composants + maj angular

--- source/TURO/src/app/app.component.ts
+++ source/TURO/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuNavComponent } from './tu-nav/tu-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
