30a7f867660cdea2f8c2f911f85734cb50a6ee2f
Mattéo LOPES <matteo.lopes.lycee@gmail.com>
2026-09-10 10:01:40 +0200

    resolution des dépendances

--- source/TURO/src/app/app.component.ts
+++ source/TURO/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TuNavComponent } from './tu-nav/tu-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuNavComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
